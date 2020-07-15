51a70c6eb36a9c3f79f3c3ae53b0f3d2b9acbf0d
DJ Yeh <djyeh1@gmail.com>
2020-07-15 16:45:26 -0400

    connected the welcome screen UI to the code

--- src/controller/Driver.java
+++ src/controller/Driver.java
@@ -1,10 +1,14 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import model.User;
 
 /**
  * This is the main class where the main thread runs through to start the application.
@@ -13,12 +17,37 @@ import javafx.stage.Stage;
  */
 public class Driver extends Application
 {
+	//----------------------------------------Global Point of Reference-----------------------------------------
+	/**
+	 * Gives the entire application a global point of access to the stage 
+	 * in which all the UI scenes will be switched in and out of.
+	 */
+	public static Stage mainStage;
+	
+	/**
+	 * Gives the entire application a global point of access to the user
+	 * containing all the info to display throughout the application.
+	 */
+	public static User user;
+	//----------------------------------------Global Point of Reference-----------------------------------------
+	
 	/**
 	 * This is the main method where the main thread will thread through to set up the application.
 	 * @param args
 	 */
 	 public static void main(String[] args) 
 	 {
+		 //**************************************************************************
+		 // DESERIALIZATION SHOULD OCCUR HERE
+		 // NEED TO JUST DESERIALIZE THE USER TO GRAB BACK ALL THE INFO FROM THE 
+		 // LAST SESSION.
+		 // |
+		 // |
+		 // \/
+		 
+		 
+		 
+		 //**************************************************************************
 		 launch(args);
 		 //this line basically sets up another thread that will set up a bunch of behind the scenes
 		 //stuff to set up UI which eventually calls the start method which is overridden with our definition below
@@ -30,10 +59,30 @@ public class Driver extends Application
 	 @Override
 	 public void start(Stage primaryStage) throws Exception
 	 {
-		 Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
+		 mainStage = primaryStage;
+		 
+		 //if-else, if there is a user object already, just show the today's tdl, else show the welcome screen
+		 Parent root = FXMLLoader.load(getClass().getResource("/view/welcome_screen.fxml"));
 		 //the param at the end details where the UI gets its format from when displaying things
-	     primaryStage.setTitle("Hello World");
-	     primaryStage.setScene(new Scene(root, 300, 275));
+	     primaryStage.setTitle("YAM To Do List");
+	     primaryStage.setScene(new Scene(root));
+	     primaryStage.setResizable(false);
 	     primaryStage.show();
+	     
+		 // --------------------------------------EVENT HANDLER----------------------------------------------
+		 // this serializes all objects
+		 // this sets an event handler for the primary stage when it's closed
+		 mainStage.setOnCloseRequest(e -> {
+			 try 
+			 {
+			 	 //User.writeToFile();
+			 } 
+			 catch (Exception err) 
+			 {
+				 System.out.println("Error serializing objects.");
+			 }
+
+		 });
+		// -------------------------------------------------------------------------------------------------
 	 }
 }
---
+++ src/controller/WelcomeController.java
@@ -0,0 +1,97 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+/**
+ * This is the controller for the /view/welcome_screen.fxml file.
+ * @author DJ Yeh and Sherry Shao
+ */
+public class WelcomeController 
+{
+	
+	@FXML
+	private Button continueBtn;
+	
+	@FXML
+	private Button darkThemeBtn;
+	
+	@FXML
+	private Button rosyThemeBtn;
+	
+	@FXML
+	private Button lightThemeBtn;
+	
+	@FXML
+	private Button fourthThemeBtn;
+	
+	@FXML
+	private Button fifthThemeBtn;
+	
+	@FXML
+	private ImageView yamLogoIV;
+	
+	@FXML
+	private TextField nameTF;
+	
+	/**
+	 * Event handler for the continueBtn button when clicked upon.
+	 * Should set up the user with his/her name, the theme, and other things.
+	 */
+	@FXML
+	private void setUpUser()
+	{
+		
+	}
+	
+	/**
+	 * Event handler for the darkThemeBtn button when clicked upon.
+	 */
+	@FXML
+	private void changeToDark()
+	{
+		
+	}
+	
+	/**
+	 * Event handler for the rosyThemeBtn button when clicked upon.
+	 */
+	@FXML
+	private void changeToRosy()
+	{
+		
+	}
+	
+	/**
+	 * Event handler for the lightThemeBtn button when clicked upon.
+	 */
+	@FXML
+	private void changeToLight()
+	{
+		
+	}
+	
+	/**
+	 * Event handler for the fourthThemeBtn button when clicked upon.
+	 */
+	@FXML
+	private void changeToFourth()
+	{
+		
+	}
+	
+	/**
+	 * Event handler for the fifthThemeBtn button when clicked upon.
+	 */
+	@FXML
+	private void changeToFifth()
+	{
+		
+	}
+	
+	
+	
+	
+}
